8938d82ef22700608ef5ea92420e45638c6e151a
Huang Shanshan <g4sunny@cdf.toronto.edu>
2016-02-28 19:28:55 -0500

    finished part 3

--- code/lm_prob.m
+++ code/lm_prob.m
@@ -50,10 +50,7 @@ function logProb = lm_prob(sentence, LM, type, delta, vocabSize)
   % TODO: once upon a time there was a curmudgeonly orangutan named Jub-Jub.
   logProb = 0;
   for index=1:(length(words) - 1)
-      prev_word_count = LM.uni.words{index};
-      if (prev_word_count ~= 0) and (delta == 0)
-          logProb = logProb + log2(LM.bi.words{index}.words{index+1} / LM.uni.words{index});
-      elseif (prev_word_count ~= 0)
+      if (isfield(LM.uni, words{index})) && (isfield(LM.bi.words{index}.words{index+1}))
           logProb = logProb + log2((LM.bi.words{index}.words{index+1} + delta) / (LM.uni.words{index} + delta * vocabSize));
       else
           logProb = -Inf;
