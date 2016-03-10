c8b1617ca896f9a7125768fff7b7228022d0fb89
Huang Shanshan <g4sunny@cdf.toronto.edu>
2016-03-09 23:18:30 -0500

    changed

--- code/evalAlign.m
+++ code/evalAlign.m
@@ -10,20 +10,21 @@ testDir      = '/u/cs401/A2_SMT/data/Hansard/Testing/Task5';
 fn_LME       = '~/lmtraineng';
 fn_LMF       = '~/lmtrainfre';
 lm_type      = '';
-delta        = TODO; %(float) smoothing parameter where 0<delta<=1 
-vocabSize    = TODO; %(integer) the number of words in the vocabulary
-numSentences = 1000;% 10000, 15000, 30000}; %(integer) The maximum number of training sentences to consider.
+%delta        = TODO; %(float) smoothing parameter where 0<delta<=1 
+%vocabSize    = TODO; %(integer) the number of words in the vocabulary
+%numSentences = 1000;% 10000, 15000, 30000}; %(integer) The maximum number of training sentences to consider.
 AMFEDir      = '~/AMFE';
-maxIter      = 20;
+maxIter      = 10;
 
+%[status, result] = unix('env LD_LIBRARY_PATH='''' curl -u "0011056e-c82a-4dea-8354-c697c607a580":"6izojpCZMrdJ" -X POST -F "text=Dans le monde reel, il n''y a rien de mal a cela." -F "source=fr" -F "target=en" "http://gateway.watsonplatform.net/language-translation/api/v2/translate"')
 
 
 % Train your language models. This is task 2 which makes use of task 1
-LME = lm_train( trainDir, 'e', fn_LME );
-LMF = lm_train( trainDir, 'f', fn_LMF );
+%LME = lm_train( trainDir, 'e', fn_LME );
+%LMF = lm_train( trainDir, 'f', fn_LMF );
 
 % Train your alignment model of French, given English 
-AMFE = align_ibm1(trainDir, numSentences, maxIter, AMFEDir);
+%AMFE = align_ibm1(trainDir, numSentences, maxIter, AMFEDir);
 
 % ... TODO: more  build moreeeeeeeeee models
 
@@ -33,7 +34,25 @@ AMFE = align_ibm1(trainDir, numSentences, maxIter, AMFEDir);
 %====================== Finishing vuilding model ===================
 
 % upload test french and english data line by line
-test_text = upload(strcat(testDir, '.f'), 'f');
+
+test_text = {};
+ibm_translation = {};
+
+fre_file = fopen(strcat(testDir, '.f'));
+i = 1;
+
+while (~feof(fre_file))
+    test_text{i} = fgets(fre_file);
+    test_text{i} = regexprep( test_text{i}, '''', '''''');
+    curl_command = sprintf('env LD_LIBRARY_PATH='''' curl -u "0011056e-c82a-4dea-8354-c697c607a580":"6izojpCZMrdJ" -X POST -F "text=%s" -F "source=fr" -F "target=en" "https://gateway.watsonplatform.net/language-translation/api/v2/translate"', test_text{i});
+    [status, ibm_translation{i}] = unix(curl_command);
+    i = i + 1;
+end  
+fclose(fre_file);
+
+for i=1:length(ibm_translation)
+    disp(ibm_translation{i})
+end
 google_result = upload(strcat(testDir, '.google.e'), 'e');
 hansard_result = upload(strcat(testDir, '.e'), 'e');
 
@@ -61,16 +80,4 @@ hansard_score = hansard_correct/total;
 
 % TODO: perform some analysis
 % add BlueMix code here 
-
-
-
-
-
-
-
-
-
-
-
-[status, result] = unix('');
 
